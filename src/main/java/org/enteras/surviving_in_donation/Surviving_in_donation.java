5a7de01fcdf347514bdf33feaccda1492d586d13
ŁotaŔ <155155069+Lotar473@users.noreply.github.com>
2024-04-28 16:16:59 +0900

    Project Start

---
+++ src/main/java/org/enteras/surviving_in_donation/Surviving_in_donation.java
@@ -0,0 +1,17 @@
+package org.enteras.surviving_in_donation;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Surviving_in_donation extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
