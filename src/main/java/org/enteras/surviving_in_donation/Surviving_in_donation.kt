900d34d0d51d19e5e97e5a6dcf435be4dd46cb46
ŁotaŔ <155155069+Lotar473@users.noreply.github.com>
2024-04-28 16:19:21 +0900

    Surviving_in_donation.kt (converted Java file to Kotlin)

--- src/main/java/org/enteras/surviving_in_donation/Surviving_in_donation.kt
+++ src/main/java/org/enteras/surviving_in_donation/Surviving_in_donation.kt
@@ -1,17 +1,13 @@
-package org.enteras.surviving_in_donation;
+package org.enteras.surviving_in_donation
 
-import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.java.JavaPlugin
 
-public final class Surviving_in_donation extends JavaPlugin {
-
-    @Override
-    public void onEnable() {
-        // Plugin startup logic
-
+class Surviving_in_donation : JavaPlugin() {
+    override fun onEnable() {
+        getLogger().info("I LIKE MONEY! GIVE ME NONEY!")
     }
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+    override fun onDisable() {
+        getLogger().info("Damn it! Plugin disabled!")
     }
 }
